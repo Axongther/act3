57b8ca3c98bf0aade3b95e2bc37b1fd6e0f7bfea
Axongther <noa2006117@gmail.com>
2025-04-11 09:50:33 -0600

    Fix formatting of cidr_blocks in Linux Web Server security group

--- mainterraform.tf
+++ mainterraform.tf
@@ -90,7 +90,7 @@ resource "aws_security_group" "linux_web_sg_1" {
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
-    cidr_blocks = [ "0.0.0.0/0 "]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
