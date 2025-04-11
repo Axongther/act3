6b17d11452e19a311ee50ee9286fe0d8b572c40d
Axongther <noa2006117@gmail.com>
2025-04-11 09:41:08 -0600

    Update ingress rule for Linux Web Server security group to allow traffic from all IPs

--- mainterraform.tf
+++ mainterraform.tf
@@ -90,7 +90,7 @@ resource "aws_security_group" "linux_web_sg_1" {
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
-    security_groups = [aws_security_group.linux_jump_sg.id]
+    cidr_blocks = [ "0.0.0.0/0 "]
   }
 
   egress {
